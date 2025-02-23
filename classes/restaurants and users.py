a766933d035fa443e77eea5b87c8bceacb49bcac
maroc <ya7yatiba@gmail.com>
2025-02-23 23:42:24 +0000

    finalized the restaurant project

--- classes/restaurants and users.py
+++ classes/restaurants and users.py
@@ -15,10 +15,15 @@ class Restaurant:
         print(f"The restaurant {self.restaurant_name}")
 
     #served clients related functions
+
+    # this function serves the simple purpose of printing the value of served clients
     def read_served_clients(self):
         print(f"A total of {self.number_served} clients have been served!")
+
+    # this function assigns the served clients a fresh value
     def change_served_clients(self,to_change_value):
         self.number_served = to_change_value
+    # increment the served clients value
     def increment_served_client(self,addition):
         self.number_served += addition
 
@@ -41,14 +46,25 @@ class User:
         self.last_name = last_name
         self.age = age
         self.adress = adress
+      
         self.favorite_food = favorite_food
+        self.logging_attempts = 0    
     # user method
     def describe(self):
         print(f"{self.first_name} {self.last_name} is {self.age} years old, his adress is {self.adress},and his favorite food is {self.favorite_food}")
     def greet(self):
         print(f"Greetings, {self.first_name} {self.last_name} , welcome to the coding enviroment!")
 
-
+    # incrementation of logging attempts
+    def read_logging_attempts(self):    
+        print(f"The total amount of logging attempts is: {self.logging_attempts}")
+    def increment_loggings(self):
+        self.logging_attempts += 1
+    
+    #reset the loggings counter
+    def loggings_reset(self):
+        self.logging_attempts = 0
+    
 
 
 
@@ -69,3 +85,17 @@ mike.describe()
 jackson.describe()
 mike.greet()
 jackson.greet()
+
+my_restaurant.change_served_clients(3)
+my_restaurant.read_served_clients()
+my_restaurant.increment_served_client(50)
+my_restaurant.increment_served_client(30)
+my_restaurant.read_served_clients()
+
+
+mike.increment_loggings()
+mike.increment_loggings()
+mike.increment_loggings()
+mike.read_logging_attempts()
+mike.loggings_reset()
+mike.read_logging_attempts()

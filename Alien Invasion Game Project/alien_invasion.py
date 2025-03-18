d70f8e3ccf53afb4f0951a616ba2faad83dbbec1
maroc <ya7yatiba@gmail.com>
2025-03-18 23:32:53 +0000

    ship control

--- Alien Invasion Game Project/alien_invasion.py
+++ Alien Invasion Game Project/alien_invasion.py
@@ -10,7 +10,10 @@ class AlienInvasion:
 
         pygame.init() #initializing py game methods
         self.settings = Settings()  #object referance on an end to the settings class on settings.py
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height)) # this variable contains the screen object, we also set the initial screen resolution
+                                            #this means take coordinates from left top to down right
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN) # this variable contains the screen object, we also set the initial screen resolution
+        self.settings.screen_width  = self.screen.get_rect().width  #assign settings class width to full screen's
+        self.settings.screen_height = self.screen.get_rect().height #assign settings class height to full screen's
         pygame.display.set_caption("Alien Invasion")
 #(255,255,255) equals to the colors (reg,green,blue)
         self.bg_colors = (self.settings.bg_color) # setting a color object, later to be used as the background color of our game
@@ -33,19 +36,43 @@ class AlienInvasion:
                     sys.exit()
                 #key down case
                 elif event.type == pygame.KEYDOWN: # conditioning for a key stroke event
-                    if event.key == pygame.K_RIGHT: # conditioning for the key stroke being right arrow key
-                        self.ship.moving_right =  True 
-                    elif event.key == pygame.K_LEFT: # key stroke being left arrow
-                        self.ship.moving_left = True
+                    self._check_keydown_event(event)
 
                 #key up case
                 elif event.type == pygame.KEYUP: #case of key up
-                    if event.key == pygame.K_RIGHT: # the key up being right arrow
-                          self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT: # the key up being left arrow
-                         self.ship.moving_left = False
+                    self._check_keyup_event(event)
+                
+
+
+
+
+
+    def _check_keydown_event(self,event):
+        if event.key == pygame.K_RIGHT: # conditioning for the key stroke being right arrow key
+            self.ship.moving_right =  True 
+        elif event.key == pygame.K_LEFT: # key stroke being left arrow
+            self.ship.moving_left = True
+            #game exits if q is pressed
+        elif event.key == pygame.K_q:
+             sys.exit()
+
+
+
+
+
+
+
+    def _check_keyup_event(self,event):
+        if event.key == pygame.K_RIGHT: # the key up being right arrow
+                self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT: # the key up being left arrow
+                self.ship.moving_left = False 
     
     
+
+
+
+
 
     def _upldate_screen(self):
             self.screen.fill(self.bg_colors) #this is the actual function that changes
--- Alien Invasion Game Project/ship.py
+++ Alien Invasion Game Project/ship.py
@@ -2,24 +2,31 @@
 #the initial vriables are used to draw the image in screen,
 #the rectaguled variables are used to locate and control the images more efficiently
 import pygame
+from settings import Settings
 class Ship: 
     def __init__(self,ai_game): # the ai_game parameter is supposed to have a screen object
         # ai_game is just a way to access functions of the received class
         self.screen = ai_game.screen # storing screen object of the alien invasion game
+        self.settings = ai_game.settings # settings class referenciation
         self.screen_rect = self.screen.get_rect() #we define the screen object above as rectangular
+        
+        
         
         self.image = pygame.image.load(r"C:\Users\maroc\OneDrive\Desktop\graphics\space_ship1.png") #storing the ship image
         self.rect = self.image.get_rect()# we object the ship image above as rectangle
-
+        
+        
         #
         self.rect.midbottom = self.screen_rect.midbottom #setting ship rectangular coordinates to midbottom
         self.moving_right = False # a function that checks If the ship is moving right
-        self.moving_left =  False
+        self.moving_left =  False 
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1  # move ship by 1 pixel to the right
-        if self.moving_left:
-            self.rect.x -= 1  # move ship by 1 pixel to the left
-
+            #If right key down and coordinates of ship didnt reach right screen edge
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed  # move ship by 1 pixel to the right
+            #If left key down and coordinates of ship didnt reach left screen edge
+        if self.moving_left and self.rect.left > self.screen_rect.left :
+            self.rect.x -= self.settings.ship_speed  # move ship by 1 pixel to the left
+        
     def blitme(self):
         self.screen.blit(self.image,self.rect) # drawing the ship image in the rectangular coordinates
